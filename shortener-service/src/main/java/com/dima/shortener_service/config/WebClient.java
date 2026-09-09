f184455cd1bb5f31a163b951915c18ba404e32bf
luri1337 <dkustov1337@gmail.com>
2026-09-09 12:25:21 +0500

    feat: add HTTP timeout for analytics service and local dev setup

--- shortener-service/src/main/java/com/dima/shortener_service/config/WebClient.java
+++ shortener-service/src/main/java/com/dima/shortener_service/config/WebClient.java
@@ -3,7 +3,10 @@ package com.dima.shortener_service.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClient {
@@ -12,8 +15,15 @@ public class WebClient {
 
     @Bean
     public RestClient analyticsWebClient(){
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory();
+
+        factory.setConnectionRequestTimeout(Duration.ofSeconds(3));
+        factory.setReadTimeout(Duration.ofSeconds(3));
+
         return RestClient.builder()
                 .baseUrl(analyticsServiceUrl)
+                .requestFactory(factory)
                 .build();
     }
 }
