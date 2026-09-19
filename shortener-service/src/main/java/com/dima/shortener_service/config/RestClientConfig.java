b86ec32c22d59709573757808ada35511b5c811f
luri1337 <dkustov1337@gmail.com>
2026-09-19 13:45:56 +0500

    fix: fix RestClient config key and add proper timeouts

--- shortener-service/src/main/java/com/dima/shortener_service/config/RestClientConfig.java
+++ shortener-service/src/main/java/com/dima/shortener_service/config/RestClientConfig.java
@@ -1,25 +1,45 @@
 package com.dima.shortener_service.config;
 
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.core5.util.Timeout;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
-import java.time.Duration;
-
 @Configuration
 public class RestClientConfig {
-    @Value("${analytics.service.url}")
+    @Value("${analytics.service-url}")
     private String analyticsServiceUrl;
 
     @Bean
     public RestClient analyticsWebClient(){
-        HttpComponentsClientHttpRequestFactory factory =
-                new HttpComponentsClientHttpRequestFactory();
+        ConnectionConfig connectionConfig = ConnectionConfig.custom()
+                .setConnectTimeout(Timeout.ofSeconds(5))
+                .setSocketTimeout(Timeout.ofSeconds(5))
+                .build();
 
-        factory.setConnectionRequestTimeout(Duration.ofSeconds(3));
-        factory.setReadTimeout(Duration.ofSeconds(3));
+        PoolingHttpClientConnectionManager connectionManager =
+                new PoolingHttpClientConnectionManager();
+        connectionManager.setDefaultConnectionConfig(connectionConfig);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(Timeout.ofSeconds(5))
+                .setResponseTimeout(Timeout.ofSeconds(5))
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory factory =
+                new HttpComponentsClientHttpRequestFactory(httpClient);
 
         return RestClient.builder()
                 .baseUrl(analyticsServiceUrl)
